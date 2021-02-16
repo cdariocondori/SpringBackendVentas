1e7d0e88ded383c7e71a8883f1e12ad51286366e
dario <dxrkio@gmail.com>
2021-02-16 01:40:01 -0400

    adicion del repository cliente

---
+++ src/main/java/com/example/demo/repository/IClienteRepository.java
@@ -0,0 +1,8 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IClienteRepository extends JpaRepository<Cliente, Integer> {
+
+}
